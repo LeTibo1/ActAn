916f448df2ee841245f619ef568d4c4cccd14626
Thibaut Göritz <thibautgoritz@vpn511-161.rzuser.uni-heidelberg.de>
2026-09-26 18:52:36 +0200

    Added debugger

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,5 @@
 import sys
+import traceback
 import parser as ps
 import data_extractor as de
 import area_picker as ap
@@ -8,7 +9,7 @@ def main():
     args = ps.parse_args()
     
     # 1. extraction
-    file = "oxydase2.csv"
+    file = "activitétotale3.csv"
     if file:
         dts, data = de.run_extraction(file)
 
@@ -19,10 +20,12 @@ def main():
     # 3. Plot both plots and calculate velocity
     if data_area:
         v = pg.run_plot_generation(file, data, data_area)
+        print("v = ", v)
 
 if __name__ == "__main__":
     try:
         main()
     except Exception as e:
         print(f"Error found: {e}", file=sys.stderr)
+        traceback.print_exc()
         sys.exit(1)
